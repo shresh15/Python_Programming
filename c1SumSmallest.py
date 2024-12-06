9d2765b25faa16717ef2069615684d2b7266c8ee
shrestha <shresthachakrabortykgp@gmail.com>
2024-12-06 13:31:31 +0530

    Sum of first 5 smallest

---
+++ c1SumSmallest.py
@@ -0,0 +1,11 @@
+numbers=[12,32,55,67,3,55,68,22,55,89,55,1,19,32]
+
+nums=list(set(numbers))
+nums.sort()
+print(nums)
+# print(nums[0]+nums[1]+nums[2])
+# for i in nums:
+#     sum=sum+
+summ=sum(nums[:5])
+print(summ)
+
