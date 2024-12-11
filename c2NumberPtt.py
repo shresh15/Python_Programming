af3b927705516db766743f8bc99cc0e600e78dce
shrestha <shresthachakrabortykgp@gmail.com>
2024-12-11 21:04:36 +0530

    number pattern

---
+++ c2NumberPtt.py
@@ -0,0 +1,13 @@
+
+n = (int)(input("enter number "))
+for i in range(1, n + 1):  
+        
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+        
+    for j in range(2, i + 1):
+        print(j, end=" ")
+    print()  
+
+
+
