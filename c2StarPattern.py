b6e1f9355e995f6c1b1ffe276b83d7f31f4e2b5e
shrestha <shresthachakrabortykgp@gmail.com>
2024-12-11 21:09:34 +0530

    Star pattern printing

---
+++ c2StarPattern.py
@@ -0,0 +1,13 @@
+
+
+n = (int)(input("enter number "))
+for i in range(1, n + 1):  
+    for j in range(1, i + 1):  
+        print("*", end=" ")  
+    print()  
+#for i in range()
+p=n-1
+for i in range(p, 0, -1):  
+    for j in range(1, i + 1):  
+        print("*", end=" ")  
+    print()  
